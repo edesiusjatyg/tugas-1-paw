19979a58dc2e7b219656e056d49c209a86f59c3b
edesiusjatyg <edesiusjatyg@gmail.com>
2026-03-02 02:04:34 +0700

    fix: vite rollup options for multi page applications

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
     base: "/tugas-1-paw/",
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                personalLogs: resolve(__dirname, 'personal-logs.html')
+            }
+        }
+    }
 });
